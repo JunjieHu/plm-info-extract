7f5d2ddc26a6798e796abdf6609d3aa449c64086
NafisaRaisa <96096118+NafisaRaisa@users.noreply.github.com>
2022-08-21 01:25:50 -0400

    Delete Training-Model directory

--- Training-Model/split.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-#copy the first 4796 lines to train.tsvn from data.tsv
-#skip the first line since it is the header
-sed -n 2,4797p data.tsv>train.tsv
-
-#copy the last 534 lines to test.tsv from data.tsv
-tail -n 535 data.tsv>test.tsv
